6a84a1dd55961ced6a544df593bf248c4f8df552
Yanzhi Bai <yanzhi.bai@affirm.com>
2016-07-20 17:50:30 -0700

    deco and gevent

--- deco/mydeco2.py
+++ deco/mydeco2.py
@@ -1,15 +1,27 @@
 def d(fp):
     def _d(*args, **kwargs):
-        print "Arguments were: %s, %s" % (args, kwargs)
+        print "Arguments were: {}, {}".format(args, kwargs)
         print "do sth before fp.."
-        r = fp(*args, **kwargs)
+        ret = fp(*args, **kwargs)
         print "do sth after fp.."
-        return r;
+        return ret;
     return _d
 
 @d
 def f(i):
-    print "in f() with %s" % i
+    print "in f() with {}".format(i)
 
 f(2)
 f([2, 3])
+
+"""
+~/repos/python-practice/deco(master) python mydeco2.py
+Arguments were: (2,), {}
+do sth before fp..
+in f() with 2
+do sth after fp..
+Arguments were: ([2, 3],), {}
+do sth before fp..
+in f() with [2, 3]
+do sth after fp..
+"""
