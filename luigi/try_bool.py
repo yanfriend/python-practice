32d00570f6abcdb98908af04354bb1eba747d7ee
yanfriend <baifriend@gmail.com>
2016-09-13 17:09:37 -0700

    luigi dynamic parameter not

--- luigi/try_bool.py
+++ luigi/try_bool.py
@@ -5,16 +5,17 @@ import time
 # this is a bad example. 
 # BoolParameter instead of BooleanParameter(depreciated), which default value is False
 # use --foo to pass in True.
-
+# luigi does not support dynamic parameter as num = luigi.IntParameter(default = (0,1)[int(foo)])
 class Boo(luigi.Task):
-    foo = luigi.BoolParameter(default=True)
+    foo = luigi.BoolParameter()
+    num = luigi.IntParameter(default = (0,1)[int(foo)])
 
     def output(self):
         return luigi.LocalTarget(path='/tmp/master-%d.txt' % 0)
 
     def run(self):
         # import ipdb; ipdb.set_trace()
-        print self.foo
+        print self.foo, self.num
 
 if __name__ == '__main__':
     luigi.run()
