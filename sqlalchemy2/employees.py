0cac7c15c6ce33e4898f0aaad13d5716b8ba0d77
yanfriend <baifriend@gmail.com>
2016-10-25 17:05:37 -0700

    more backref, uselist

--- sqlalchemy2/employees.py
+++ sqlalchemy2/employees.py
@@ -9,16 +9,13 @@ Base = declarative_base()
 class Department(Base):
     __tablename__ = 'department'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(64))
  
  
 class Employee(Base):
     __tablename__ = 'employee'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    # Use default=func.now() to set the default hiring time
-    # of an Employee to be the current time when an
-    # Employee record was created
+    name = Column(String(64))
     hired_on = Column(DateTime, default=func.now())
     department_id = Column(Integer, ForeignKey('department.id'))
     # Use cascade='delete,all' to propagate the deletion of a Department onto its Employees
@@ -30,7 +27,8 @@ class Employee(Base):
     # Dept accesses employees use list 
  
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///orm_in_detail.sqlite')
+# engine = create_engine('sqlite:///orm_in_detail.sqlite')
+engine = create_engine('mysql://root:@localhost:3306/test', echo=True)
  
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker()
@@ -43,11 +41,12 @@ it_department = Department(name='IT')
 john.department = it_department
 
 session.add(john)
-# session.add(it_department)
 session.commit()
 
-it = session.query(Department).filter(Department.name == 'IT').one()
+it = session.query(Department).filter(Department.name == 'IT').first()
 print it.employees
 print it.employees[0].name
 
+# session.delete(it)
+# session.commit()
 
